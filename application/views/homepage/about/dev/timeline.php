c5e30b929a052fd74032b32b241cfa41f7726ff9
안형모 <hamen1219@naver.com>
2022-04-20 16:14:26 +0900

    TEST : Disabled timeline page
    temp..

--- application/views/homepage/about/dev/timeline.php
+++ application/views/homepage/about/dev/timeline.php
@@ -8,63 +8,7 @@
 
 	<div id = "contents">
 
-		<div class = "div">		
-			<h2 class = "word_title"><b> 이력</b></h2>	
-			<img id = "time_img" src="/img/about/timeline.png">
 
-		</div>
-
-		<div class = "div">
-			<h2 class = "word_title"><b> 활동자료</b></h2>		
-			
-			<table>
-				<tr>
-					<td>
-						<div class = "img_wrap">
-							<h3>졸업작품 출품 (`18.12 - `19.10)</h3>
-							<img src="/img/about/img.png">	
-							<img src="/img/about/iot.png">	
-
-						</div>
-													
-					</td>
-				</tr>
-
-				<tr>
-					<td>
-						<div class = "img_wrap">
-							<h3>PHP 유치원 싸이트 제작 (`20.4 -`20.5)</h3>
-							<img src="/img/about/img1.png">	
-							<img src="/img/about/img3.png">	
-						</div>
-													
-					</td>
-				</tr>
-
-				<tr>
-					<td>
-						<div class = "img_wrap">
-							<h3>이데아 커뮤니티 제작 (`20.9 - `20.12)</h3>
-							<img src="/img/about/img2.png">	
-						</div>
-													
-					</td>
-				</tr>
-
-			
-			</table>
-			
-		</div>
-	</div>
-
-
-
-		
-
-		
-
-		
-		
 	</div>
 </section>
 
